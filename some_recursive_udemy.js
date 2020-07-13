87d76a7c463432d8fe7dd291da0ed9185cdf59f0
Crew Spence <csspence123@gmail.com>
2020-07-13 07:20:05 -0500

    Wrote working solution.

--- some_recursive_udemy.js
+++ some_recursive_udemy.js
@@ -5,8 +5,16 @@ someRecursive
 Write a recursive function called someRecursive which accepts an array and a callback.  The function returns true if a single value in the array returns true when passed to the callback.  Otherwise it returns false.
 */
 
-function someRecursive(){
-
+const someRecursive = (array, callback) => {
+  if(callback(array[array.length - 1]) === true) {
+    return true;
+  }
+  if(array.length > 1) {
+    return someRecursive(array.slice(0, array.length - 1), callback);
+  }
+  if(array.length === 1) {
+    return false;
+  }
 }
 
 // SAMPLE INPUT / OUTPUT
